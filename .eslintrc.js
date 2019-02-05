d39750fd1c269ad93415e88e8f368594117fc393
ranjayzheng <ranjayzheng@qq.com>
2019-02-05 14:57:46 +0800

    install all dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,19 @@
 module.exports = {
-    "extends": "standard"
+    "extends": "standard",
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module" //es6 import
+    },
+    "globals": {
+        "fetch": false,
+        "define": false
+    },
+    "rules": {
+        "no-unreachable": 2,
+        "no-console": 0
+    },
+    "rules": {
+        "indent": ["error", 4]
+    }
 };
